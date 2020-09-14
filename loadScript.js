667ddb6eb5169edb2810e16bb5580c9f2d77c4f7
Uday <udayphani.k@maybank.com>
2020-09-14 16:08:11 +0800

    Commit with changes to single css style

--- loadScript.js
+++ loadScript.js
@@ -2,13 +2,12 @@ document.getElementById('pageTitle').append(window.location.pathname);
 let checkSessStorage = JSON.parse(sessionStorage.getItem('loadChecks'));
 enableIcon();
 function enableIcon() {
-     let enableIcon = document.getElementsByClassName('loadChecks');
      for (let i = 0; i < checkSessStorage.length; i++) {
           var element = document.getElementById('lcIcon');
           if (window.location.pathname === checkSessStorage[i].uri_type && checkSessStorage[i].flag === 'ON') {
                element.classList.remove("dispnone");
                element.classList.toggle("loadChecks");
-               return
+               return;
           }
      }
 }
